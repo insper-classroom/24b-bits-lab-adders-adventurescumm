b523535cadbe882903db1292d84eaccbe8ec06c5
adventurescumm <103777055+adventurescumm@users.noreply.github.com>
2024-09-05 12:41:08 +0000

    Até adder2bits concluido

--- ula_modules.py
+++ ula_modules.py
@@ -11,29 +11,47 @@ from myhdl import *
 def halfAdder(a, b, soma, carry):
     @always_comb
     def comb():
-        pass
+        soma.next = a^b
+        carry.next = a*b
 
     return instances()
 
 
 @block
 def fullAdder(a, b, c, soma, carry):
+    s = [Signal(bool(0)) for i in range(3)]
+    haList = [None for i in range(2)]  # (1)
+
+    haList[0] = halfAdder(a, b, s[0], s[1]) 
+    haList[1] = halfAdder(c, s[0], soma, s[2])
+
     @always_comb
     def comb():
-        pass
+        carry.next = s[1] | s[2]
 
     return instances()
 
 
+
+
 @block
 def adder2bits(x, y, soma, carry):
+    s = Signal(bool(0))
+
+    half = halfAdder(x[0], y[0], soma[0], s)
+    full = fullAdder(x[1], y[1], s, soma[1], carry)
+
+
     return instances()
 
 
 @block
 def adder(x, y, soma, carry):
-    @always_comb
-    def comb():
-        pass
+    n = len(x)
+    s = [Signal(bool(0)) for i in range(len(soma))]
+    faList = [None for i in range(n)]
+
+    for n in range(len(soma)):
+        faList[n] = fullAdder(x[n], y[n], s[n], soma[n], carry[n])
 
     return instances()
